d69d65f714ce83eb079402e60335a04133c7f689
Gang Song <songgang@seas.upenn.edu>
2013-05-30 19:12:53 -0400

    add tar script to extract paraview mha.gz and vtk.gz files for uploading

---
+++ cuneyt/gzip_vtk_from_directories.sh
@@ -0,0 +1,40 @@
+# oneline script to get all .vtk.gz into a tar file
+# cd /home/songgang/project/Cuneyt/Jan2012/output_Mar13_2013
+/bin/tar -cvf Mar13_vtk.tar */*-30cm/LobeMasks/ParaviewFiles/*.mha.gz */*-mov-10cm/LobeMasks/ParaviewFiles/*PS1Moving.vtk.gz */*-mov-10cm/LobeMasks/ParaviewFiles/*TotalWarpMoving.vtk.gz
+
+
+# GILMER-PRE-SUPINE-30cm.mha
+
+# LLL-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# LLL-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# LML-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# LML-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# LUL-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# LUL-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# RCL-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# RCL-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# RLL-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# RLL-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# RML-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# RML-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# RUL-GILMER-PRE-SUPINE-30cm_fulllobe.mha
+
+# RUL-GILMER-PRE-SUPINE-30cm_vessels.mha
+
+# GILMER-PRE-SUPINE-PS1Moving.vtk
+
+# GILMER-PRE-SUPINE-TotalWarpMoving.vtk
+
+# STATE-GILMER-PRE-SUPINE.pvsm
